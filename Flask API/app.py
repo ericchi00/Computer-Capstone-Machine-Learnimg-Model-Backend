3eb6125f55011197945fba9c84f284d6a9f798aa
Eric Chi <98287442+ericchi00@users.noreply.github.com>
2022-12-30 15:56:36 -0800

    added via upload
    removed flashai depedency

--- Flask API/app.py
+++ Flask API/app.py
@@ -2,39 +2,37 @@ import io
 import json
 from urllib.request import urlopen, Request
 
-import requests
 from PIL import Image
-from flask import Flask, request, jsonify
+from flask import Flask, request
 from flask_cors import CORS
-from tensorflow import keras
+from numpy import argmax
+from tensorflow import keras, expand_dims, nn
 
 app = Flask(__name__)
 CORS(app)
+model_dir = "./Model"
+model = keras.models.load_model(model_dir, compile=False)
 
 
 def convert(url):
     request_site = Request(url, headers={"User-Agent": "Mozilla/5.0"})
     image_url = urlopen(request_site)
     image_bytes = io.BytesIO(image_url.read())
-    image = Image.open(image_bytes)
-    return image
+    img = Image.open(image_bytes).convert('RGB').resize((300, 300))
+    return img
 
 
 @app.route("/", methods=["POST"])
 def main():
     json_data = request.get_json()
-    image = convert(json_data['url']).resize((300, 300))
-    # testing local image
-    # image = keras.preprocessing.image.image_utils.load_img("Test/IMG_2872.JPEG", target_size = (256, 256))
-    image = keras.preprocessing.image.img_to_array(image)
-    request_message = json.dumps({"model": "ericchi_7728de", "input": image.tolist(), "verbose": 1})
-    inference_url = "https://flashai.io/serve"
-    request_headers = {"content-type": "application/json"}
-    response = requests.post(inference_url, headers=request_headers, data=request_message)
-    prediction = response.content.decode("ascii", "ignore")
-    probability = prediction.replace("[", "").replace("]", "").split()
-    return jsonify(probability)
+    img = convert(json_data['url'])
+    img_array = keras.preprocessing.image.img_to_array(img)
+    img_array = expand_dims(img_array, 0)
+    prediction = model.predict(img_array).tolist()
+    score = nn.softmax(prediction[0])
+    class_probabilities = [argmax(score), prediction[0]]
+    return json.dumps(class_probabilities, default=str)
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(port=5000)
